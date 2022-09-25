8139c1843d588e7352e25494682769363f28835a
Miles Sigel <mgs6@rice.edu>
2022-09-25 17:26:42 -0500

    added blink led

--- python/simple_calc.py
+++ python/simple_calc.py
@@ -3,7 +3,7 @@
 Simple Calculator
 --------------------------------------------------------------------------
 License:   
-Copyright 2022 - <NAME>
+Copyright 2022 - Miles Sigel
 
 Redistribution and use in source and binary forms, with or without 
 modification, are permitted provided that the following conditions are met:
